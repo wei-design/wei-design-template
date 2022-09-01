6ca1dd845257ce9ebbf963aec5a5ac3fab077ec8
forguo <forguo@dazwang.com>
2022-09-01 23:59:33 +0800

    feat(*): 添加组件库配置，添加按钮组件
    添加组件库配置，添加按钮组件

---
+++ packages/components/button/src/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <button :class="[classString]" :disabled="disabled">
+      <slot></slot>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import './index.scss'
+
+const Props = {
+    type: {
+        type: String as PropType<string>, // 转为ts类型string
+        default: 'default'
+    },
+    disabled: {
+        type: Boolean as PropType<boolean>,
+        default: false
+    },
+}
+
+export default defineComponent({
+    name: 'WeButton',
+    props: Props,
+    setup(props) {
+        const classString = [
+            'we-button',
+            `we-button-type-${props.type}`,
+            props.disabled ? 'we-button-disabled' : ''
+        ]
+        return {
+            classString
+        }
+    }
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
 
-import './assets/main.css'
+import WeDesign from "../packages/index"; // 可以配置alias
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.use(WeDesign).mount("#app");
